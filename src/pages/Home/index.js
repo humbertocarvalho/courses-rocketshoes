3ca69c4b72ba0034a8d60e45e60b83bd70e77428
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-09 19:18:50 -0300

    Created home component.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,122 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="FF">
+              3
+            </MdAddShoppingCart>
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="FF">
+              3
+            </MdAddShoppingCart>
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="FF">
+              3
+            </MdAddShoppingCart>
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="FF">
+              3
+            </MdAddShoppingCart>
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="FF">
+              3
+            </MdAddShoppingCart>
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="FF">
+              3
+            </MdAddShoppingCart>
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            {/* TODO Verificar por que não consigo adicionar a quantidade como texto aqui */}
+            <MdAddShoppingCart size={16} color="FFF" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
