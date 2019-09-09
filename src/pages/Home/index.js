3b806499ef51c32092134241e4d95c65e197f5b6
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-09 19:49:38 -0300

    Created api.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,122 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
-          alt="Tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="FF">
-              3
-            </MdAddShoppingCart>
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
-          alt="Tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="FF">
-              3
-            </MdAddShoppingCart>
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
-          alt="Tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="FF">
-              3
-            </MdAddShoppingCart>
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
-          alt="Tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="FF">
-              3
-            </MdAddShoppingCart>
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
-          alt="Tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="FF">
-              3
-            </MdAddShoppingCart>
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
-          alt="Tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="FF">
-              3
-            </MdAddShoppingCart>
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_zoom1.jpg"
-          alt="Tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            {/* TODO Verificar por que não consigo adicionar a quantidade como texto aqui */}
-            <MdAddShoppingCart size={16} color="FFF" />
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get('products');
+    this.setState({
+      products: response.data,
+    });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="FF">
+                  3
+                </MdAddShoppingCart>
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
